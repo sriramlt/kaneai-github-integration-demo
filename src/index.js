5f58f4d1bcbe513e053e3450c904bdb75380a050
Sriram K <sriram@lambdatest.com>
2025-12-02 15:26:39 -0600

    Add /hello/:name endpoint and update README

--- src/index.js
+++ src/index.js
@@ -3,5 +3,12 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send('Hello from LambdaTest sample app'));
+
 app.get('/api/echo', (req, res) => res.json({ echo: req.query.msg || 'hello' }));
+
+app.get('/hello/:name', (req, res) => {
+  const name = req.params.name;
+  res.send(`Hello, ${name}!`);
+});
+
 app.listen(port, () => console.log(`App running on http://localhost:${port}`));
